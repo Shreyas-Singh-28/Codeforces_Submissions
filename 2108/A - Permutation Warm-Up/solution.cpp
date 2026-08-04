14c458cb1e4c8509ca297dd037d69eaaac4d56c4
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:39:01 +0530

    Add Permutation Warm-Up [A] from Codeforces

---
+++ 2108/A - Permutation Warm-Up/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=int64_t;
+ 
+void solve() {
+    int n;
+    cin>>n;
+    cout<<(int)pow(n,2)/4+1<<endl;
+}
+ 
+int main() {    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int tt;
+    cin>>tt;
+    while(tt--) {
+        solve();
+    }
+    return 0;
+}
