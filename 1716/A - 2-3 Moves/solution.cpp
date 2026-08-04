377dbe03cd24436987e34088ae4485f6038c60d3
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:48:31 +0530

    Add 2-3 Moves [A] from Codeforces

---
+++ 1716/A - 2-3 Moves/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    if(n==1) {
+        cout<<2;
+    }
+    else{
+        cout<<(n+2)/3;
+    }
+    cout<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
