1cf155bf0cd192b42231f316a6923c55781eef93
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:51:01 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(auto &x:a)cin>>x;
+    if(a[0]==1) cout<<"YES"<<'
+';
+    else cout<<"NO"<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
