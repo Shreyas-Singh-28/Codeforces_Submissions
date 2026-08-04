a9c38a3a875ecd5b76ea0ed6c55611350bb8eaf4
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:37:20 +0530

    Add Young Physicist [A] from Codeforces

---
+++ 69/A - Young Physicist/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+ 
+int main() {
+    short n;
+    cin>>n;
+    if(n<1||n>100)
+        return 0;
+    short arr[n][3];
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<3;j++) {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<3;i++) {
+        int sum=0;
+        for(int j=0;j<n;j++) {
+            sum+=arr[j][i];
+        }
+        if(sum!=0) {
+            cout<<"NO";
+            return 0;
+        }
+    }
+    cout<<"YES";
+    return 0;
+}
