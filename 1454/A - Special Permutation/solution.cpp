661c0a5af12370839c3ba1928585e38277c023e0
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:43:30 +0530

    Add Special Permutation [A] from Codeforces

---
+++ 1454/A - Special Permutation/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    for(int i=1;i<n;i++) cout<<i+1<<" ";
+    cout<<1<<'
+';
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
