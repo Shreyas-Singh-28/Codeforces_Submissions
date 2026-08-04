14d45fe770f1b51ba9eaebff4ee5dc852b44d488
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:52:07 +0530

    Add Divide and Conquer [A] from Codeforces

---
+++ 2241/A - Divide and Conquer/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int x,y;
+    cin>>x>>y;
+    if(x%y==0) {
+        cout<<"YES"<<'
+';
+        return;
+    }
+    cout<<"NO"<<'
+';
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
