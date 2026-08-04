e1dd9110109703224cc2ccef77417989c250874f
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:46:35 +0530

    Add False Alarm [A] from Codeforces

---
+++ 2117/A - False Alarm/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n,x;cin>>n>>x;
+    vector<int> a(n);
+    for(auto &x:a)cin>>x;
+    int i=0;
+    for(;i<n;i++) {
+        if(a[i]==1) {
+            break;
+        }
+    }
+    i+=x;
+    for(;i<n;i++) {
+        if(a[i]==1) {
+            cout<<"NO
+";return;
+        }
+    }
+    cout<<"YES
+";
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
