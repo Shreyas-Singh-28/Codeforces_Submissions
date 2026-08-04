ee97cc695f464302c0d3e731b2fa8cfbeafbd86e
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:48:13 +0530

    Add Dice Roll Sequence [C] from Codeforces

---
+++ 2195/C - Dice Roll Sequence/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(auto &x:a)cin>>x;
+    int c=0;
+    for(int i=0;i<n-1;i++) {
+        if(a[i]==a[i+1] || a[i]+a[i+1]==7) {
+            c++;
+            i++;
+        }
+    }
+    cout<<c<<'
+';
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
