cb6eb9816b1539ac95c49ea5bb0ff9e9598e68e1
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:49:17 +0530

    Add Celex Update [C] from Codeforces

---
+++ 1358/C - Celex Update/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    ll x1,y1,x2,y2;cin>>x1>>y1>>x2>>y2;
+    // y2%=99999;
+    // y1%=99999;
+    // x1%=99999;
+    // x2%=99999;
+    // int x=0,y=0;
+    cout<<(x2-x1)*(y2-y1)+1<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
