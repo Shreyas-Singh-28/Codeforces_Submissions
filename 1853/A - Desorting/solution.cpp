067da522f52119565bec1ddea0568c99af62f7a5
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:50:30 +0530

    Add Desorting [A] from Codeforces

---
+++ 1853/A - Desorting/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(auto &x:a)cin>>x;
+    int ans=INT_MAX;
+    for(int i=1;i<n;i++) {
+        if(a[i-1]>a[i]) {
+            cout<<0<<'
+';
+            return;
+        }
+        ans=min(ans,a[i]-a[i-1]);
+    }
+    cout<<(ans+2)/2<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
