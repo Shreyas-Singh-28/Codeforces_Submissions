c81c6362bc00be8ff6aa0d422e22af7329dc1c16
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:50:56 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    if(n%3==0) cout<<"Second"<<'
+';
+    else cout<<"First"<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
