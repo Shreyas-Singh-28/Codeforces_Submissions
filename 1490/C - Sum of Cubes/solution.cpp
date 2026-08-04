96659e7ee480535c8a315f83983568d1760f9a19
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:47:30 +0530

    Add Sum of Cubes [C] from Codeforces

---
+++ 1490/C - Sum of Cubes/solution.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    ll n;cin>>n;
+    ll i=1;
+    while(n-(i*i*i)>0) {
+        // double d=n-(i*i*i);
+        ll num=n-(i*i*i);
+        ll root=round(cbrt(num));
+        if(num==(root*root*root)) {
+            cout<<"YES
+";
+            return;
+        }
+        i++;
+    }
+    cout<<"NO
+";
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
