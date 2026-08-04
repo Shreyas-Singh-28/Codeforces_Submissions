16c8f9b808ebda11ddf323b90b68a69988cd0dfa
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:53:35 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;
+    cin>>n;
+    int c=1;
+    while(n--) {
+        cout<<c<<' ';
+        c+=2;
+    }
+    cout<<'
+';
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
