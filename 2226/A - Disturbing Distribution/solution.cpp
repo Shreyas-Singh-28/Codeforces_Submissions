508f1a05f36ace8d06ca4db9de74d54c70bedac1
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-04 17:47:50 +0530

    Add Disturbing Distribution [A] from Codeforces

---
+++ 2226/A - Disturbing Distribution/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+const int MOD67 = 676767677;
+ 
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(auto &x:a) cin>>x;
+    int c=0;
+    int i=0;
+    for(;i<n;i++) {
+        if(a[i]!=1) {
+            c=(c+a[i])%MOD67;
+        }
+        else{
+            while(i<n-1 && a[i]==1) {
+                i++;
+            }
+            c=(c+a[i])%MOD67;
+        }
+    }
+    cout<<c<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
