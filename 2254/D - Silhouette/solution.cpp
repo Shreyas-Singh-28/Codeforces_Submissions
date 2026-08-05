8549ea26b097e8b48961c9f508791506bc10ab75
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-06 00:28:37 +0530

    Add Silhouette [D] from Codeforces

--- 2254/D - Silhouette/solution.cpp
+++ 2254/D - Silhouette/solution.cpp
@@ -5,6 +5,20 @@ const int INF = 1e9;
 const ll LLINF = 1e18;
 const int MOD = 1e9+7;
  
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+ 
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+ 
 // 0 0 4 4 14
 // 2 2 5 5 6
  
@@ -24,7 +38,7 @@ void solve() {
 ';
         return;
     }
-    unordered_map<ll,ll> mp;
+    unordered_map<ll,ll,custom_hash> mp;
     ll last=0;
     for(int i=0;i<n;i++) {
         int terms=1;
