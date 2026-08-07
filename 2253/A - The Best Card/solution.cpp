902ab93ab7f25b9903e20b59c14ca334583ddd1e
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-07 20:10:01 +0530

    Add The Best Card [A] from Codeforces

---
+++ 2253/A - The Best Card/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    for(int i=2;i*i<=n+1;i++) {
+        if((n+1)%i==0) {
+            cout<<"NO
+";
+            return;
+        }
+    }
+    cout<<"YES
+";
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
