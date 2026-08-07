83872be24bef3abe80e213207a0f087286e068f0
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-07 14:13:31 +0530

    Add Boss Fight [A] from Codeforces

--- 2252/A - Boss Fight/solution.cpp
+++ 2252/A - Boss Fight/solution.cpp
@@ -5,44 +5,24 @@ const int INF = 1e9;
 const ll LLINF = 1e18;
 const int MOD = 1e9+7;
  
-// 10 5 4 10 10 10 
-// 10 10 10
- 
-void solve() {
-    int n;cin>>n;
-    vector<int> a(n);
-    map<int,int,greater<int>> mp;
-    for(int &x:a) {
-        cin>>x;
-        mp[x]++;
-    }
-    ll res=0;
-    int last=-1;
-    bool ok=0;
-    while(n>0) {
-        int val=-1;
-        int max_freq=0;
-        for(auto au=mp.begin();au!=mp.end() && n>0;au++) {
-            if(au->second!=0 && au->first!=last)  {
-                if(au->second>max_freq) {
-                    max_freq=au->second;
-                    val=au->first;
-                }
-            }
-        }
-        if(val==-1) {
-            val=last;
-            ok=1;
-        }
-        res+=val;
-        mp[val]--;
-        last=val;
-        n--;
-        if(ok) break;
-    }
-    cout<<res<<'
-';
-    return;
+void solve(){
+	int n; cin >> n;
+	ll total = 0;
+	map<ll, int> freq;
+	ll maxx = 0, ele = -1;
+	for(int i=0; i<n; i++){
+		ll x; cin >> x;
+		total += x;
+		freq[x]++;
+		if(freq[x] > maxx){
+			maxx = freq[x];
+			ele = x;
+		}
+	}
+	ll cur = 2*maxx - n - 2;
+	if(cur>0) total -= cur * ele;
+	cout << total << "
+";
 }
  
 int main() {
