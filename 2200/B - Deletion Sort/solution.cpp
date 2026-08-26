2a09c563e7ab1d1487b22b562c5c9f49049f2b0f
Sheyas Singh Rajkumar <shreyas2006singh@gmail.com>
2026-08-26 23:16:11 +0530

    Add Deletion Sort [B] from Codeforces

---
+++ 2200/B - Deletion Sort/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+const int MOD = 1e9+7;
+ 
+void solve() {
+    int n;cin>>n;
+    vector<int> a(n);
+    for(int &x:a) cin>>x;
+    if(n==1) {
+        cout<<1<<'
+';
+        return;
+    }
+    for(int i=0;i<n-1;i++) {
+        if(a[i]>a[i+1]) {
+            cout<<1<<'
+';
+            return;
+        }
+    }
+    cout<<n<<'
+';
+    return;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--) solve();
+}
